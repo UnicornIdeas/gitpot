f8160208882323c4ab17147bfee14e9e43613497
Helmeseanu Alexandru <helmeseanualexandru@gmail.com>
2021-10-12 21:19:05 +0300

    ..

--- clay_front/src/main.js
+++ clay_front/src/main.js
@@ -2,6 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
+import firebase from 'firebase'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjD5-MT-lSqtQoOc7snVGHHqkSkHjrtMY",
+  authDomain: "clay-netw.firebaseapp.com",
+  projectId: "clay-netw",
+  storageBucket: "clay-netw.appspot.com",
+  messagingSenderId: "844980183975",
+  appId: "1:844980183975:web:f5dff8df67e0ef043dcc82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false
 
 new Vue({
