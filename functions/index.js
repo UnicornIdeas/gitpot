a19b9483efd2e223a073491439bf62c22e2ef06b
Helmeseanu Alexandru <helmeseanualexandru@gmail.com>
2021-10-17 21:14:09 +0300

    functie update firestore

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,3 @@ admin.initializeApp();
 const qr=require("./queryregistry.js");
 
 exports.queryregistry=qr.queryregistry;
-// exports.scheduledFunction = functions.pubsub.schedule("every 5 seconds").onRun((context) => {
-//   console.log("This will be run every 5 minutes!");
-//   return null;
-// });
